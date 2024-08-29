af4fbb7779a358a8f290a98b753db7f9095123a4
Dmitrii <efremov.dima.2016@gmail.com>
2024-08-29 08:30:10 +0300

    Add config.Util Realization UserJdbcRepository

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
--- src/main/java/com/dsvdev/Main.java
+++ src/main/java/com/dsvdev/Main.java
@@ -1,7 +1,22 @@
 package com.dsvdev;
 
+import com.dsvdev.repository.UserJdbcRepository;
+
 public class Main {
     public static void main(String[] args) {
-        //your code here
+        UserJdbcRepository userRepository = new UserJdbcRepository();
+
+        userRepository.createUsersTable();
+
+        userRepository.saveUser("Dmitrii_One", "Test1", (byte) 19);
+        userRepository.saveUser("Dmitrii_Two", "Test2", (byte) 50);
+        userRepository.saveUser("Dmitrii_Three", "Test3", (byte) 100);
+        userRepository.saveUser("Dmitrii_Four", "Test4", (byte) 1);
+
+        userRepository.getAllUsers();
+
+        userRepository.cleanUsersTable();
+
+        userRepository.dropUsersTable();
     }
 }
